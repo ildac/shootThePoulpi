bf58dfd1350c2ef8010fd1dd4dab74c3f43aff3f
dac <marco@dacol.me>
2013-11-23 09:36:12 +0100

    add basic enemy spawn

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -6,22 +6,68 @@ using System.Collections;
 /// </summary>
 public class EnemyScript : MonoBehaviour {
 
+	private bool hasSpawn = false;
+	private MoveScript moveScript;
 	private WeaponScript[] weapons;
 
 	void Awake() {
 		// Retrieve the weapon only once
 		weapons = GetComponentsInChildren<WeaponScript> ();
+
+		// Retreive the script to disable when not spawn
+		moveScript = GetComponent<MoveScript> ();
 	}
-	
+
+	void Start () {
+		// Disable everything
+		// - collider
+		collider2D.enabled = false;
+		// - moving
+		moveScript.enabled = false;
+		// - shooting
+		foreach (WeaponScript weapon in weapons) {
+			weapon.enabled = false;
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
-		foreach (WeaponScript weapon in weapons)
+
+		if (hasSpawn == false) 
 		{
-			// Auto-fire
-			if (weapon != null && weapon.CanAttack) 
+			if (renderer.isVisibleFrom (Camera.main)) 
 			{
-				weapon.Attack(true);
+				Spawn ();
 			}
+		} else 
+		{
+			foreach (WeaponScript weapon in weapons) 
+			{
+				// Auto-fire
+				if (weapon != null && weapon.CanAttack) 
+				{
+					weapon.Attack (true);
+				}
+			}
+
+			if (renderer.isVisibleFrom(Camera.main) == false) 
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	private void Spawn () {
+		hasSpawn = true;
+
+		// Enable EveryThing
+		// - collider
+		collider2D.enabled = true;
+		// - moving
+		moveScript.enabled = true;
+		// - shooting
+		foreach (WeaponScript weapon in weapons) {
+			weapon.enabled = true;
 		}
 	}
 }
