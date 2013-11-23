0324260d67ceb3c5846a11d1aac7fa8db90f9aed
dac <marco@dacol.me>
2013-11-23 10:42:13 +0100

    player is now limited to the view size boundary

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,7 +7,7 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour {
 
 	/// <summary>
-	/// 1 - the speed of the ship
+	/// The speed of the ship
 	/// </summary>
 	public Vector2 speed = new Vector2(50,50);
 
@@ -16,23 +16,23 @@ public class PlayerScript : MonoBehaviour {
 	}
 	
 	void Update () {
-		// 2 - retrive axys information
+		// Retrive axys information
 		float inputX = Input.GetAxis ("Horizontal");
 		float inputY = Input.GetAxis ("Vertical");
 
-		// 3 - movement per direction
+		// Movement per direction
 		Vector3 movement = new Vector3 (
 			speed.x * inputX,
 			speed.y * inputY,
 			0);
 
-		// 4 - Relative to the time
+		// Relative to the time
 		movement *= Time.deltaTime;
 
-		// 5 - Move the object in game
+		// Move the object in game
 		transform.Translate (movement);
 
-		// 6 - Shooting
+		// Shooting
 		bool shoot = Input.GetButtonDown ("Fire1");
 		shoot |= Input.GetButtonDown ("Fire2");
 
@@ -41,10 +41,32 @@ public class PlayerScript : MonoBehaviour {
 			WeaponScript weapon = GetComponent<WeaponScript>();
 			if (weapon != null)
 			{
-				// false because the player is not an enemy
+				// False because the player is not an enemy
 				weapon.Attack(false);
 			}
 		}
+
+		// Keep player inside camera bounds
+		// Since all of these methods works only with the center of the sprite,
+		// here i get the size of the bounding box of the sprite and reducing the
+		// boundaries of the screen i manage to keep all the ship in the view
+		Vector3 playerSize = renderer.bounds.size;
+
+		// Here is the definition of the boundary in world point
+		var distance = (transform.position - Camera.main.transform.position).z;
+
+		var leftBorder = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance)).x + (playerSize.x/2);
+		var rightBorder = Camera.main.ViewportToWorldPoint (new Vector3 (1, 0, distance)).x - (playerSize.x/2);
+
+		var bottomBorder = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, distance)).y + (playerSize.y/2);
+		var topBorder = Camera.main.ViewportToWorldPoint (new Vector3 (0, 1, distance)).y - (playerSize.y/2);
+	
+		// Here the position of the player is clamped into the boundaries
+		transform.position = (new Vector3 (
+			Mathf.Clamp (transform.position.x, leftBorder, rightBorder),
+			Mathf.Clamp (transform.position.y, bottomBorder, topBorder),
+			transform.position.z)
+		);
 
 	}
 }
